6f6626cc8c4eb59d61d338bba24740cebb3c454e
recoma96 <seokbong60@gmail.com>
2025-11-08 15:27:00 +0900

    feat: mypy 타입체크 도입
    container class 수정
    create_app 함수 생성

--- server/api/src/trailine_api/main.py
+++ server/api/src/trailine_api/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 
+from trailine_api.container import Container
 from trailine_api.routers import router as api_router
 
 
-app = FastAPI(title=__name__)
+def create_app() -> FastAPI:
+    container = Container()
+
+    app = FastAPI(title="trailine_api")
+    app.container = container  # type: ignore[attr-defined]
+    app.include_router(api_router, prefix="/api")
+    return app
 
 
-app.include_router(api_router, prefix="/api")
+app = create_app()
