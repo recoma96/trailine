84ab672aeee598774f6fdfc36dc2c63d86a817e8
recoma96 <seokbong60@gmail.com>
2025-11-10 21:56:03 +0900

    feat: place 테이블 생성
    PostGIS관련 테이블들은 마이그레이션 대상에서 제외하게 env 수정
    geoalchemy2 설치 (PostGIS타입 사용 목적)

---
+++ server/model/src/trailine_model/models/place.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+from geoalchemy2 import Geometry, Geography
+
+from trailine_model.base import Base, TimeStampModel
+
+
+class Place(Base, TimeStampModel):
+    __tablename__ = "place"
+    __table_args__ = {
+        "comment": "장소"
+    }
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(16), nullable=False)
+    geom: Mapped[Any] = mapped_column(Geography(geometry_type="POINT", srid=4326), nullable=False,
+                                      comment="위경도(뷰포트 검색용)")
+    geog: Mapped[Any] = mapped_column(Geometry(geometry_type="POINT", srid=4326), nullable=False,
+                                      comment="위경도(거리, 버퍼 연산)")
+    elevation: Mapped[int] = mapped_column(Integer, nullable=True, comment="해발고도(m)")
+    land_address: Mapped[str] = mapped_column(String(128), nullable=True, comment="지번 주소")
+    road_address: Mapped[str] = mapped_column(String(128), nullable=True, comment="도로명 주소")
+    description: Mapped[str] = mapped_column(Text, nullable=True, comment="설명")
