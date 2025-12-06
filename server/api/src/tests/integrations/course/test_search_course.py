4a9839260761f0d22cb4eff28810387bb5f33eeb
recoma96 <seokbong60@gmail.com>
2025-12-06 18:32:46 +0900

    refactor: test setup 함수 공통 모듈화

--- server/api/src/tests/integrations/course/test_search_course.py
+++ server/api/src/tests/integrations/course/test_search_course.py
@@ -5,53 +5,11 @@ from starlette.testclient import TestClient
 import pytest
 from httpx import Response
 
-from tests.data.course_intervals import POINTS_A_TO_B_DATA, POINTS_B_TO_C_DATA
-from tests.data.places import (
-    PLACE_A_DATA,
-    PLACE_B_DATA,
-    PLACE_C_DATA
-)
-from tests.factories import (
-    PlaceFactory,
-    CourseIntervalDifficultyFactory,
-    CourseIntervalFactory,
-    CourseFactory,
-    CourseDifficultyFactory,
-    CourseStyleFactory,
-)
+from tests.integrations.common import setup_course_data_no_1
 
 
 def _setup_data(session: Session):
-    # given
-    place_a = PlaceFactory.create(**PLACE_A_DATA)
-    place_b = PlaceFactory.create(**PLACE_B_DATA)
-    place_c = PlaceFactory.create(**PLACE_C_DATA)
-
-    difficulty_lv1 = CourseIntervalDifficultyFactory.create(level=1)
-    difficulty_lv2 = CourseIntervalDifficultyFactory.create(level=2)
-
-    interval_a_to_b = CourseIntervalFactory.create(
-        difficulty=difficulty_lv1,
-        place_a=place_a,
-        place_b=place_b,
-        points=POINTS_A_TO_B_DATA
-    )
-    interval_b_to_c = CourseIntervalFactory.create(
-        difficulty=difficulty_lv2,
-        place_a=place_b,
-        place_b=place_c,
-        points=POINTS_B_TO_C_DATA
-    )
-
-    CourseFactory.create(
-        name="관악산 일부 코스",
-        course_difficulty=CourseDifficultyFactory.create(),
-        course_style=CourseStyleFactory.create(),
-        links=[
-            {"interval": interval_a_to_b, "position": 1},
-            {"interval": interval_b_to_c, "position": 2}
-        ]
-    )
+    setup_course_data_no_1()
     session.commit()
 
 
