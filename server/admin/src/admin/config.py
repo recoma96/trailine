4c5542062106116ba13df6149ad4e5994a8979e5
recoma96 <seokbong60@gmail.com>
2025-12-06 19:42:52 +0900

    feat: 코스 구간 이미지 관리 페이지 추가

--- server/admin/src/admin/config.py
+++ server/admin/src/admin/config.py
@@ -16,12 +16,14 @@ class _DevConfig(_CommonConfig):
     class S3(_CommonConfig.S3):
         BASE_PLACE_PATH = "dev/public/place"
         BASE_COURSE_PATH = "dev/public/course"
+        BASE_COURSE_INTERVAL_PATH = "dev/public/course-interval"
 
 
 class _ProdConfig(_CommonConfig):
     class S3(_CommonConfig.S3):
         BASE_PLACE_PATH = "public/place"
         BASE_COURSE_PATH = "public/course"
+        BASE_COURSE_INTERVAL_PATH = "public/course-interval"
 
 
 IS_TEST: bool = (os.getenv("IS_TEST") == "1")
