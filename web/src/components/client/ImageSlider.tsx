ea401c9f777c26735999488951b4b648d50d7c49
recoma96 <seokbong60@gmail.com>
2025-12-12 05:23:15 +0900

    feat: 상단부분 구현
    제목, 주소, 난이도, 이미지 슬라이드, 소개

---
+++ web/src/components/client/ImageSlider.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+
+type Image = {
+    title: string;
+    description: string | null;
+    url: string;
+};
+
+interface ImageSliderProps {
+    images: Image[];
+}
+
+const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
+    return (
+        <div className="max-w-[800px] mx-auto">
+            <Swiper
+                loop={true}
+                cssMode={true}
+                navigation={true}
+                pagination={{ clickable: true }}
+                mousewheel={true}
+                keyboard={true}
+                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                className="mySwiper [--swiper-navigation-color:#fff] [--swiper-pagination-color:#fff]"
+            >
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="relative w-full h-full">
+                            <div className="top-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+                                {image.title}
+                            </div>
+                            <img 
+                                src={image.url} 
+                                alt={image.title} 
+                                className="w-full h-auto max-h-[600px] object-contain block"
+                            />
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
+};
+
+export default ImageSlider;
