bdd72710375f5236e02392c7013d02d6bd70b620
recoma96 <seokbong60@gmail.com>
2026-02-21 08:02:58 +0900

    refactor: 기상청 발표시각 선택하는 알고리즘 util에서 kma_mountains.py로 이동

--- server/api/src/trailine_api/integrations/weather/kma_mountain.py
+++ server/api/src/trailine_api/integrations/weather/kma_mountain.py
@@ -10,7 +10,6 @@ from trailine_api.integrations.weather.schemas import (
     KmaMountainWeatherCode,
     KmaMountainWeatherItemParsed,
 )
-from trailine_api.integrations.weather.utils import get_latest_kma_announcement_dt
 from trailine_api.repositories.weather_repositories import IWeatherRepository
 from trailine_api.settings import Settings
 
@@ -51,7 +50,7 @@ class KmaMountainWeather(IWeatherProvider):
         return self._parse_short_term_forecast_data(response.data, target_dt)
 
     def _build_request_params(self, mountain_num: int, target_dt: datetime) -> Dict[str, Any]:
-        announcement_dt = get_latest_kma_announcement_dt(target_dt)
+        announcement_dt = self._get_latest_kma_announcement_dt(target_dt)
 
         return {
             "mountainNum": mountain_num,
@@ -132,3 +131,29 @@ class KmaMountainWeather(IWeatherProvider):
             KmaMountainWeatherCode.WIND_DIRECTION: builder.set_wind_direction,
             KmaMountainWeatherCode.WIND_SPEED: builder.set_wind_speed,
         }.get(category, None)
+
+    def _get_latest_kma_announcement_dt(self, target_dt: datetime) -> datetime:
+        """
+        KMA 발표 시각(02, 05, 08, 11, 14, 17, 20, 23) 중 target_dt와 가장 가까운 과거의 시각을 반환합니다.
+        """
+        announcement_hours = [2, 5, 8, 11, 14, 17, 20, 23]
+
+        # 현재 시간의 시(hour) 정보를 가져옴
+        current_hour = target_dt.hour
+
+        # 발표 시간들 중 current_hour보다 작거나 같은 가장 큰 값을 찾음
+        latest_hour = -1
+        for hour in reversed(announcement_hours):
+            if current_hour >= hour:
+                latest_hour = hour
+                break
+
+        # 만약 current_hour가 02시 이전이면 전날 23시로 설정
+        if latest_hour == -1:
+            announcement_dt = target_dt - timedelta(days=1)
+            announcement_dt = announcement_dt.replace(hour=23, minute=0, second=0, microsecond=0)
+        else:
+            announcement_dt = target_dt.replace(hour=latest_hour, minute=0, second=0, microsecond=0)
+
+        return announcement_dt
+
